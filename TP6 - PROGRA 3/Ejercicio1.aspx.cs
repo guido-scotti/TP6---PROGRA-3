eea0903270101dec1e31a8852e11c53b78feb83d
guidoScotti <scottigui@gmail.com>
2025-05-07 12:45:47 -0300

    inicio de ej 1

---
+++ TP6 - PROGRA 3/Ejercicio1.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Data;
+
+namespace TP6___PROGRA_3
+{
+	public partial class WebForm2 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            if (!IsPostBack)
+            {
+                CargarProductos();
+            }
+        }
+
+        private void CargarProductos()
+        {
+            GestionNeptuno gestion = new GestionNeptuno();
+            DataTable tabla = gestion.ObtenerProductos(); // Asegurate que esto devuelva los datos actualizados
+            gridProductos.DataSource = tabla;
+            gridProductos.DataBind();
+        }
+
+        protected void gridProductos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gridProductos_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string idProducto = ((Label)gridProductos.Rows[e.RowIndex].FindControl("lbl_item_idprod")).Text;
+            Productos producto = new Productos(Convert.ToInt32(idProducto));
+            GestionNeptuno gestion = new GestionNeptuno();
+            gestion.eliminarProducto(producto);
+            CargarProductos();
+        }
+
+        protected void gridProductos_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gridProductos.EditIndex = e.NewEditIndex;
+            CargarProductos();
+        }
+
+        protected void gridProductos_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gridProductos.EditIndex = -1;
+            CargarProductos(); 
+        }
+
+
+        protected void gridProductos_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            // Obtener los controles desde la fila que se está editando
+            string idTexto = ((Label)gridProductos.Rows[e.RowIndex].FindControl("lbl_it_idProducto"))?.Text;
+            string nuevoNombre = ((TextBox)gridProductos.Rows[e.RowIndex].FindControl("txtNombre"))?.Text;
+            string nuevoPrecioTexto = ((TextBox)gridProductos.Rows[e.RowIndex].FindControl("txtPrecio"))?.Text;
+            string nuevaCantidadTexto = ((TextBox)gridProductos.Rows[e.RowIndex].FindControl("txtCantidad"))?.Text;
+
+   
+            if (!int.TryParse(idTexto, out int id))
+            {
+               // lblError.Text = "El ID del producto no es válido.";
+                return;
+            }
+
+            if (!decimal.TryParse(nuevoPrecioTexto, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal nuevoPrecio))
+            {
+                //lblError.Text = "El precio ingresado no es válido.";
+                return;
+            }
+
+            if (!int.TryParse(nuevaCantidadTexto, out int nuevaCantidad))
+            {
+                //lblError.Text = "La cantidad ingresada no es válida.";
+                return;
+            }
+
+       
+            Productos producto = new Productos(id, nuevoNombre, nuevoPrecio, nuevaCantidad);
+
+       
+            GestionNeptuno gestion = new GestionNeptuno();
+            gestion.ActualizarProducto(producto);
+
+        
+            gridProductos.EditIndex = -1;
+            CargarProductos();
+        }
+    }
+}
---
+++ TP6 - PROGRA 3/INICIO.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP6___PROGRA_3
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control lblGrupo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblGrupo;
+
+        /// <summary>
+        /// Control hypEjercicio1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypEjercicio1;
+
+        /// <summary>
+        /// Control hypEjercicio2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypEjercicio2;
+    }
+}
